230f44a977d535898a26a6072cf90cd7019d31b4
Vedant Raut <vedantraut49@gmail.com>
2026-01-17 11:12:49 +0530

    first commit of pensieve

---
+++ Pensieve/src/main/java/com/vedant/pensieve/PensieveApplication.java
@@ -0,0 +1,13 @@
+package com.vedant.pensieve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PensieveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PensieveApplication.class, args);
+	}
+
+}
